71209acc7a238dfbd4b52ebbc85aab2b279034ed
Mats Macke <matsmacke@gmail.com>
2021-11-17 14:53:41 +0100

    Copy-pasted code from old project to get started

---
+++ src/editor.rs
@@ -0,0 +1,115 @@
+use crate::Terminal;
+use crate::Document;
+use termion::event::Key;
+
+pub struct Position {
+    x: usize,
+    y: usize
+}
+
+enum Mode {
+    Normal,
+    Insert,
+    Select
+}
+
+pub struct Editor {
+    should_quit: bool,
+    terminal: Terminal,
+    cursor: Position,
+    scroll: usize,
+    mode: Mode,
+}
+
+impl Editor {
+    pub fn run(&mut self) {
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(&error);
+            }
+
+            // Stop the program if the should_quit flag is "true"
+            if self.should_quit {
+                break
+            }
+
+            if let Err(error) = self.process_keypress() {
+                die(&error);
+            }
+        }
+    }
+
+    pub fn default() -> Self {
+        Editor{
+            should_quit: false,
+            terminal: Terminal::default().expect("Terminal initialisation failed."),
+            cursor: Position {x: 0, y: 0},
+            scroll: 0,
+            mode: Mode::Normal,
+        }
+    }
+
+    /// Do all the required screen drawing
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::show_cursor(false);
+        Terminal::move_cursor_to(0, 0);
+        if self.should_quit {
+            println!("Have a nice day :))");
+            Terminal::show_cursor(true);
+        }
+        else {
+            self.terminal.draw_brackets();
+            self.display_cursor();
+            print!("{}", self.cursor.y);
+        }
+        Terminal::flush()
+    }
+
+    /// Get keypress and handle it appropriately
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = Terminal::read_key()?;
+        match self.mode {
+            Mode::Normal => {
+                match pressed_key {
+                    Key::Ctrl('q') => self.should_quit = true,
+                    Key::Up => self.move_cursor(0, -1),
+                    Key::Down => self.move_cursor(0, 1),
+                    Key::Left => self.move_cursor(-1, 0),
+                    Key::Right => self.move_cursor(1, 0),
+                    Key::Char(c) => (),
+                    _ => ()
+                }
+            },
+            Mode::Insert => {
+
+            },
+            Mode::Select => {
+
+            }
+        }
+        Ok(())
+    }
+
+    /// Move the editor cursor (not necessarily the same as `Terminal::move_cursor`)
+    fn move_cursor(&mut self, x: isize, y: isize) {
+        match x {
+            0 => {},
+            1.. => self.cursor.x = self.cursor.x.saturating_add(x as usize),
+            _ => self.cursor.x = self.cursor.x.saturating_sub((-1*x) as usize)
+        }
+        match y {
+            0 => {},
+            1.. => self.cursor.y = self.cursor.y.saturating_add(y as usize),
+            _ => self.cursor.y = self.cursor.y.saturating_sub((-1*y) as usize)
+        }
+    }
+
+    fn display_cursor(&self) {
+        Terminal::move_cursor_to(self.cursor.x as u16, self.cursor.y as u16);
+    }
+}
+
+/// In case everything goes wrong
+fn die(e: &std::io::Error) {
+    panic!("{}", e)
+}
---
+++ src/terminal.rs
@@ -0,0 +1,90 @@
+use std::io::{self, stdout, Write};
+use crate::Position;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+
+/// Simple struct to store the size of a terminal
+pub struct Size {
+    pub width: u16,
+    pub height: u16,
+}
+
+/// This struct interfaces with the terminal to display all there is to display
+pub struct Terminal {
+    size: Size,
+    _stdout: RawTerminal<std::io::Stdout>
+}
+
+impl Terminal {
+    /// Get the size of the current terminal
+    /// # Errors
+    /// No known possible errors, but better safe than sorry
+    pub fn default() -> Result<Self, std::io::Error> {
+        let size = termion::terminal_size()?;
+        Ok(Self {
+            size: Size {
+                width: size.0,
+                height: size.1,
+            },
+            _stdout: stdout().into_raw_mode()?
+        })
+    }
+
+    /// Getter for the size of the terminal
+    pub fn size(&self) -> &Size {
+        &self.size
+    }
+
+    /// Clears the whole terminal
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);
+    }
+
+    /// Clears the line the cursor is currently on
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
+
+    /// Moves the terminal cursor (not necessarily the same as the editor cursor)
+    pub fn move_cursor(x: u16, y: u16) {
+        let x = x.saturating_add(1);
+        let y = y.saturating_add(1);
+        print!("{}", termion::cursor::Goto(x, y));
+    }
+
+    pub fn move_cursor_to(x: u16, y: u16) {
+        let x = x.saturating_add(1);
+        let y = y.saturating_add(1);
+        print!("{}", termion::cursor::Goto(x, y));
+    }
+
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    /// Wait for a key to be pressed and return it
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+
+    /// Draw square brackets down the left side of the screen, my take on the vim classic
+    pub fn draw_brackets(&self) {
+        for _ in 0..self.size().height - 1 {
+            Terminal::clear_current_line();
+            println!("]\r");
+        }
+    }
+
+    pub fn show_cursor(show: bool) {
+        if show {
+            print!("{}", termion::cursor::Show);
+        } else {
+            print!("{}", termion::cursor::Hide);
+        }
+    }
+}
